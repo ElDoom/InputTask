f014a4d21826b0fc5793616f3c098f1990dc2386
David Lopez <118932088+ElDoom@users.noreply.github.com>
2023-04-27 15:57:56 -0300

    Initial commit

---
+++ Assets/Editor/ActionWindow.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+
+public class ActionWindow : EditorWindow
+{
+    string myString = "Hello World";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    // Add menu item named "My Window" to the Window menu
+    [MenuItem("Tool/Action Editor")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(ActionWindow),true,"Action Editor");
+       
+    }
+
+    private void OnEnable()
+    {
+        VisualTreeAsset visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/Action.uxml");
+        TemplateContainer templateContainer = visualTreeAsset.CloneTree();
+        rootVisualElement.Add(templateContainer);
+    }
+
+    //void OnGUI()
+    //{
+    //    GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+    //    myString = EditorGUILayout.TextField("Text Field", myString);
+
+    //    groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+    //    myBool = EditorGUILayout.Toggle("Toggle", myBool);
+    //    myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+    //    EditorGUILayout.EndToggleGroup();
+    //}
+}
